439c4bbe834e1519beb15417600ecb601513f60f
kleberLima1990 <kleber-r@outlook.com>
2026-01-19 18:42:29 -0300

    refactor and answerService

---
+++ src/main/java/com/my/answers/entity/dto/QuestionRequest.java
@@ -0,0 +1,13 @@
+package com.my.answers.entity.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record AnswerRequest(
+        @NotBlank(message = "You must to send some data")
+        @Size(max = 100, message = "Max size input is 100")
+        String request,
+
+        int questionNumber
+) {
+}
